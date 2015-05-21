05aafc862ac9e8ad1afbdd9eea950c4ae0115741
nmittler <nathanmittler@google.com>
2015-05-21 15:41:07 -0700

    Adding jetty test to Netty.

---
+++ netty/src/test/java/io/grpc/transport/netty/JettyTest.java
@@ -0,0 +1,21 @@
+package io.grpc.transport.netty;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Verifies that the Netty client works properly in the Jetty container when using TLS with ALPN.
+ */
+@RunWith(JUnit4.class)
+public class JettyTest {
+
+  @Before
+  public void setup() {
+  }
+
+  @Test
+  public void test() {
+  }
+}
